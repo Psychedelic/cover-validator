88e21f9159020535649767d7fef401683842055c
Scott Dn <scott.dn91@gmail.com>
2022-01-29 13:18:38 +0700

    linter & prettier

--- src/actor/factory.d.ts
+++ src/actor/factory.d.ts
@@ -1,115 +1,123 @@
-import type {Principal} from "@dfinity/principal";
+import type { Principal } from '@dfinity/principal';
 export interface Activity {
-  create_at: string;
-  canister_id: Principal;
-  build_status: BuildStatus;
+  'create_at' : string,
+  'canister_id' : Principal,
+  'build_status' : BuildStatus,
 }
 export interface BuildConfig {
-  updated_at: string;
-  canister_id: Principal;
-  created_at: string;
-  dfx_version: string;
-  owner_id: Principal;
-  canister_name: string;
-  commit_hash: string;
-  repo_url: string;
-  rust_version: [] | [string];
-  optimize_count: number;
+  'updated_at' : string,
+  'canister_id' : Principal,
+  'created_at' : string,
+  'dfx_version' : string,
+  'owner_id' : Principal,
+  'canister_name' : string,
+  'commit_hash' : string,
+  'repo_url' : string,
+  'rust_version' : [] | [string],
+  'optimize_count' : number,
 }
 export interface BuildConfigInfo {
-  canister_id: Principal;
-  owner_id: Principal;
+  'canister_id' : Principal,
+  'owner_id' : Principal,
 }
-export type BuildStatus = {Error: null} | {Building: null} | {Success: null} | {Pending: null};
-export type Error = {BuildInProgress: null};
+export type BuildStatus = { 'Error' : null } |
+  { 'Building' : null } |
+  { 'Success' : null } |
+  { 'Pending' : null };
+export type Error = { 'BuildInProgress' : null };
 export interface Pagination {
-  page_index: bigint;
-  data: Array<Activity>;
-  total_pages: bigint;
-  total_items: bigint;
-  is_first_page: boolean;
-  items_per_page: bigint;
-  is_last_page: boolean;
+  'page_index' : bigint,
+  'data' : Array<Activity>,
+  'total_pages' : bigint,
+  'total_items' : bigint,
+  'is_first_page' : boolean,
+  'items_per_page' : bigint,
+  'is_last_page' : boolean,
 }
 export interface PaginationInfo {
-  page_index: bigint;
-  items_per_page: bigint;
+  'page_index' : bigint,
+  'items_per_page' : bigint,
 }
 export interface Pagination_1 {
-  page_index: bigint;
-  data: Array<Verification>;
-  total_pages: bigint;
-  total_items: bigint;
-  is_first_page: boolean;
-  items_per_page: bigint;
-  is_last_page: boolean;
+  'page_index' : bigint,
+  'data' : Array<Verification>,
+  'total_pages' : bigint,
+  'total_items' : bigint,
+  'is_first_page' : boolean,
+  'items_per_page' : bigint,
+  'is_last_page' : boolean,
 }
 export interface RegisterVerification {
-  canister_id: Principal;
-  dfx_version: string;
-  owner_id: Principal;
-  canister_name: string;
-  commit_hash: string;
-  repo_url: string;
-  rust_version: [] | [string];
-  optimize_count: number;
+  'canister_id' : Principal,
+  'dfx_version' : string,
+  'owner_id' : Principal,
+  'canister_name' : string,
+  'commit_hash' : string,
+  'repo_url' : string,
+  'rust_version' : [] | [string],
+  'optimize_count' : number,
 }
-export type Result = {Ok: null} | {Err: Error};
+export type Result = { 'Ok' : null } |
+  { 'Err' : Error };
 export interface SaveBuildConfig {
-  canister_id: Principal;
-  dfx_version: string;
-  owner_id: Principal;
-  canister_name: string;
-  commit_hash: string;
-  repo_url: string;
-  rust_version: [] | [string];
-  optimize_count: number;
+  'canister_id' : Principal,
+  'dfx_version' : string,
+  'owner_id' : Principal,
+  'canister_name' : string,
+  'commit_hash' : string,
+  'repo_url' : string,
+  'rust_version' : [] | [string],
+  'optimize_count' : number,
 }
 export interface SubmitVerification {
-  canister_id: Principal;
-  dfx_version: string;
-  owner_id: Principal;
-  build_status: BuildStatus;
-  canister_name: string;
-  commit_hash: string;
-  repo_url: string;
-  rust_version: [] | [string];
-  optimize_count: number;
-  build_url: string;
-  wasm_hash: [] | [string];
+  'canister_id' : Principal,
+  'dfx_version' : string,
+  'owner_id' : Principal,
+  'build_status' : BuildStatus,
+  'canister_name' : string,
+  'commit_hash' : string,
+  'repo_url' : string,
+  'rust_version' : [] | [string],
+  'optimize_count' : number,
+  'build_url' : string,
+  'wasm_hash' : [] | [string],
 }
 export interface Verification {
-  updated_at: string;
-  updated_by: Principal;
-  canister_id: Principal;
-  dfx_version: string;
-  build_status: BuildStatus;
-  canister_name: string;
-  commit_hash: string;
-  repo_url: string;
-  rust_version: [] | [string];
-  optimize_count: number;
-  build_url: [] | [string];
-  wasm_hash: [] | [string];
+  'updated_at' : string,
+  'updated_by' : Principal,
+  'canister_id' : Principal,
+  'dfx_version' : string,
+  'build_status' : BuildStatus,
+  'canister_name' : string,
+  'commit_hash' : string,
+  'repo_url' : string,
+  'rust_version' : [] | [string],
+  'optimize_count' : number,
+  'build_url' : [] | [string],
+  'wasm_hash' : [] | [string],
 }
 export interface _SERVICE {
-  addAdmin: (arg_0: Principal) => Promise<undefined>;
-  addBuilder: (arg_0: Principal) => Promise<undefined>;
-  addValidator: (arg_0: Principal) => Promise<undefined>;
-  deleteAdmin: (arg_0: Principal) => Promise<undefined>;
-  deleteBuildConfig: (arg_0: Principal) => Promise<undefined>;
-  deleteBuilder: (arg_0: Principal) => Promise<undefined>;
-  deleteValidator: (arg_0: Principal) => Promise<undefined>;
-  getActivities: (arg_0: PaginationInfo) => Promise<Pagination>;
-  getAdmins: () => Promise<Array<Principal>>;
-  getBuildConfigById: (arg_0: Principal) => Promise<[] | [BuildConfig]>;
-  getBuildConfigValidator: (arg_0: BuildConfigInfo) => Promise<[] | [BuildConfig]>;
-  getBuildConfigs: () => Promise<Array<BuildConfig>>;
-  getBuilders: () => Promise<Array<Principal>>;
-  getValidators: () => Promise<Array<Principal>>;
-  getVerificationByCanisterId: (arg_0: Principal) => Promise<[] | [Verification]>;
-  getVerifications: (arg_0: PaginationInfo) => Promise<Pagination_1>;
-  registerVerification: (arg_0: RegisterVerification) => Promise<Result>;
-  saveBuildConfig: (arg_0: SaveBuildConfig) => Promise<undefined>;
-  submitVerification: (arg_0: SubmitVerification) => Promise<undefined>;
+  'addAdmin' : (arg_0: Principal) => Promise<undefined>,
+  'addBuilder' : (arg_0: Principal) => Promise<undefined>,
+  'addValidator' : (arg_0: Principal) => Promise<undefined>,
+  'deleteAdmin' : (arg_0: Principal) => Promise<undefined>,
+  'deleteBuildConfig' : (arg_0: Principal) => Promise<undefined>,
+  'deleteBuilder' : (arg_0: Principal) => Promise<undefined>,
+  'deleteValidator' : (arg_0: Principal) => Promise<undefined>,
+  'getActivities' : (arg_0: PaginationInfo) => Promise<Pagination>,
+  'getAdmins' : () => Promise<Array<Principal>>,
+  'getBuildConfigById' : (arg_0: Principal) => Promise<[] | [BuildConfig]>,
+  'getBuildConfigValidator' : (arg_0: BuildConfigInfo) => Promise<
+      [] | [BuildConfig]
+    >,
+  'getBuildConfigs' : () => Promise<Array<BuildConfig>>,
+  'getBuilders' : () => Promise<Array<Principal>>,
+  'getValidators' : () => Promise<Array<Principal>>,
+  'getVerificationByCanisterId' : (arg_0: Principal) => Promise<
+      [] | [Verification]
+    >,
+  'getVerifications' : (arg_0: PaginationInfo) => Promise<Pagination_1>,
+  'registerVerification' : (arg_0: RegisterVerification) => Promise<Result>,
+  'saveBuildConfig' : (arg_0: SaveBuildConfig) => Promise<undefined>,
+  'submitVerification' : (arg_0: SubmitVerification) => Promise<undefined>,
 }
--- src/actor/factory.ts
+++ src/actor/factory.ts
@@ -1,125 +1,135 @@
 // @ts-nocheck
-export const idlFactory = ({IDL}) => {
+export const idlFactory = ({ IDL }) => {
   const PaginationInfo = IDL.Record({
-    page_index: IDL.Nat64,
-    items_per_page: IDL.Nat64
+    'page_index' : IDL.Nat64,
+    'items_per_page' : IDL.Nat64,
   });
   const BuildStatus = IDL.Variant({
-    Error: IDL.Null,
-    Building: IDL.Null,
-    Success: IDL.Null,
-    Pending: IDL.Null
+    'Error' : IDL.Null,
+    'Building' : IDL.Null,
+    'Success' : IDL.Null,
+    'Pending' : IDL.Null,
   });
   const Activity = IDL.Record({
-    create_at: IDL.Text,
-    canister_id: IDL.Principal,
-    build_status: BuildStatus
+    'create_at' : IDL.Text,
+    'canister_id' : IDL.Principal,
+    'build_status' : BuildStatus,
   });
   const Pagination = IDL.Record({
-    page_index: IDL.Nat64,
-    data: IDL.Vec(Activity),
-    total_pages: IDL.Nat64,
-    total_items: IDL.Nat64,
-    is_first_page: IDL.Bool,
-    items_per_page: IDL.Nat64,
-    is_last_page: IDL.Bool
+    'page_index' : IDL.Nat64,
+    'data' : IDL.Vec(Activity),
+    'total_pages' : IDL.Nat64,
+    'total_items' : IDL.Nat64,
+    'is_first_page' : IDL.Bool,
+    'items_per_page' : IDL.Nat64,
+    'is_last_page' : IDL.Bool,
   });
   const BuildConfig = IDL.Record({
-    updated_at: IDL.Text,
-    canister_id: IDL.Principal,
-    created_at: IDL.Text,
-    dfx_version: IDL.Text,
-    owner_id: IDL.Principal,
-    canister_name: IDL.Text,
-    commit_hash: IDL.Text,
-    repo_url: IDL.Text,
-    rust_version: IDL.Opt(IDL.Text),
-    optimize_count: IDL.Nat8
+    'updated_at' : IDL.Text,
+    'canister_id' : IDL.Principal,
+    'created_at' : IDL.Text,
+    'dfx_version' : IDL.Text,
+    'owner_id' : IDL.Principal,
+    'canister_name' : IDL.Text,
+    'commit_hash' : IDL.Text,
+    'repo_url' : IDL.Text,
+    'rust_version' : IDL.Opt(IDL.Text),
+    'optimize_count' : IDL.Nat8,
   });
   const BuildConfigInfo = IDL.Record({
-    canister_id: IDL.Principal,
-    owner_id: IDL.Principal
+    'canister_id' : IDL.Principal,
+    'owner_id' : IDL.Principal,
   });
   const Verification = IDL.Record({
-    updated_at: IDL.Text,
-    updated_by: IDL.Principal,
-    canister_id: IDL.Principal,
-    dfx_version: IDL.Text,
-    build_status: BuildStatus,
-    canister_name: IDL.Text,
-    commit_hash: IDL.Text,
-    repo_url: IDL.Text,
-    rust_version: IDL.Opt(IDL.Text),
-    optimize_count: IDL.Nat8,
-    build_url: IDL.Opt(IDL.Text),
-    wasm_hash: IDL.Opt(IDL.Text)
+    'updated_at' : IDL.Text,
+    'updated_by' : IDL.Principal,
+    'canister_id' : IDL.Principal,
+    'dfx_version' : IDL.Text,
+    'build_status' : BuildStatus,
+    'canister_name' : IDL.Text,
+    'commit_hash' : IDL.Text,
+    'repo_url' : IDL.Text,
+    'rust_version' : IDL.Opt(IDL.Text),
+    'optimize_count' : IDL.Nat8,
+    'build_url' : IDL.Opt(IDL.Text),
+    'wasm_hash' : IDL.Opt(IDL.Text),
   });
   const Pagination_1 = IDL.Record({
-    page_index: IDL.Nat64,
-    data: IDL.Vec(Verification),
-    total_pages: IDL.Nat64,
-    total_items: IDL.Nat64,
-    is_first_page: IDL.Bool,
-    items_per_page: IDL.Nat64,
-    is_last_page: IDL.Bool
+    'page_index' : IDL.Nat64,
+    'data' : IDL.Vec(Verification),
+    'total_pages' : IDL.Nat64,
+    'total_items' : IDL.Nat64,
+    'is_first_page' : IDL.Bool,
+    'items_per_page' : IDL.Nat64,
+    'is_last_page' : IDL.Bool,
   });
   const RegisterVerification = IDL.Record({
-    canister_id: IDL.Principal,
-    dfx_version: IDL.Text,
-    owner_id: IDL.Principal,
-    canister_name: IDL.Text,
-    commit_hash: IDL.Text,
-    repo_url: IDL.Text,
-    rust_version: IDL.Opt(IDL.Text),
-    optimize_count: IDL.Nat8
+    'canister_id' : IDL.Principal,
+    'dfx_version' : IDL.Text,
+    'owner_id' : IDL.Principal,
+    'canister_name' : IDL.Text,
+    'commit_hash' : IDL.Text,
+    'repo_url' : IDL.Text,
+    'rust_version' : IDL.Opt(IDL.Text),
+    'optimize_count' : IDL.Nat8,
   });
-  const Error = IDL.Variant({BuildInProgress: IDL.Null});
-  const Result = IDL.Variant({Ok: IDL.Null, Err: Error});
+  const Error = IDL.Variant({ 'BuildInProgress' : IDL.Null });
+  const Result = IDL.Variant({ 'Ok' : IDL.Null, 'Err' : Error });
   const SaveBuildConfig = IDL.Record({
-    canister_id: IDL.Principal,
-    dfx_version: IDL.Text,
-    owner_id: IDL.Principal,
-    canister_name: IDL.Text,
-    commit_hash: IDL.Text,
-    repo_url: IDL.Text,
-    rust_version: IDL.Opt(IDL.Text),
-    optimize_count: IDL.Nat8
+    'canister_id' : IDL.Principal,
+    'dfx_version' : IDL.Text,
+    'owner_id' : IDL.Principal,
+    'canister_name' : IDL.Text,
+    'commit_hash' : IDL.Text,
+    'repo_url' : IDL.Text,
+    'rust_version' : IDL.Opt(IDL.Text),
+    'optimize_count' : IDL.Nat8,
   });
   const SubmitVerification = IDL.Record({
-    canister_id: IDL.Principal,
-    dfx_version: IDL.Text,
-    owner_id: IDL.Principal,
-    build_status: BuildStatus,
-    canister_name: IDL.Text,
-    commit_hash: IDL.Text,
-    repo_url: IDL.Text,
-    rust_version: IDL.Opt(IDL.Text),
-    optimize_count: IDL.Nat8,
-    build_url: IDL.Text,
-    wasm_hash: IDL.Opt(IDL.Text)
+    'canister_id' : IDL.Principal,
+    'dfx_version' : IDL.Text,
+    'owner_id' : IDL.Principal,
+    'build_status' : BuildStatus,
+    'canister_name' : IDL.Text,
+    'commit_hash' : IDL.Text,
+    'repo_url' : IDL.Text,
+    'rust_version' : IDL.Opt(IDL.Text),
+    'optimize_count' : IDL.Nat8,
+    'build_url' : IDL.Text,
+    'wasm_hash' : IDL.Opt(IDL.Text),
   });
   return IDL.Service({
-    addAdmin: IDL.Func([IDL.Principal], [], []),
-    addBuilder: IDL.Func([IDL.Principal], [], []),
-    addValidator: IDL.Func([IDL.Principal], [], []),
-    deleteAdmin: IDL.Func([IDL.Principal], [], []),
-    deleteBuildConfig: IDL.Func([IDL.Principal], [], []),
-    deleteBuilder: IDL.Func([IDL.Principal], [], []),
-    deleteValidator: IDL.Func([IDL.Principal], [], []),
-    getActivities: IDL.Func([PaginationInfo], [Pagination], ["query"]),
-    getAdmins: IDL.Func([], [IDL.Vec(IDL.Principal)], ["query"]),
-    getBuildConfigById: IDL.Func([IDL.Principal], [IDL.Opt(BuildConfig)], ["query"]),
-    getBuildConfigValidator: IDL.Func([BuildConfigInfo], [IDL.Opt(BuildConfig)], ["query"]),
-    getBuildConfigs: IDL.Func([], [IDL.Vec(BuildConfig)], ["query"]),
-    getBuilders: IDL.Func([], [IDL.Vec(IDL.Principal)], ["query"]),
-    getValidators: IDL.Func([], [IDL.Vec(IDL.Principal)], ["query"]),
-    getVerificationByCanisterId: IDL.Func([IDL.Principal], [IDL.Opt(Verification)], ["query"]),
-    getVerifications: IDL.Func([PaginationInfo], [Pagination_1], ["query"]),
-    registerVerification: IDL.Func([RegisterVerification], [Result], []),
-    saveBuildConfig: IDL.Func([SaveBuildConfig], [], []),
-    submitVerification: IDL.Func([SubmitVerification], [], [])
+    'addAdmin' : IDL.Func([IDL.Principal], [], []),
+    'addBuilder' : IDL.Func([IDL.Principal], [], []),
+    'addValidator' : IDL.Func([IDL.Principal], [], []),
+    'deleteAdmin' : IDL.Func([IDL.Principal], [], []),
+    'deleteBuildConfig' : IDL.Func([IDL.Principal], [], []),
+    'deleteBuilder' : IDL.Func([IDL.Principal], [], []),
+    'deleteValidator' : IDL.Func([IDL.Principal], [], []),
+    'getActivities' : IDL.Func([PaginationInfo], [Pagination], ['query']),
+    'getAdmins' : IDL.Func([], [IDL.Vec(IDL.Principal)], ['query']),
+    'getBuildConfigById' : IDL.Func(
+        [IDL.Principal],
+        [IDL.Opt(BuildConfig)],
+        ['query'],
+      ),
+    'getBuildConfigValidator' : IDL.Func(
+        [BuildConfigInfo],
+        [IDL.Opt(BuildConfig)],
+        ['query'],
+      ),
+    'getBuildConfigs' : IDL.Func([], [IDL.Vec(BuildConfig)], ['query']),
+    'getBuilders' : IDL.Func([], [IDL.Vec(IDL.Principal)], ['query']),
+    'getValidators' : IDL.Func([], [IDL.Vec(IDL.Principal)], ['query']),
+    'getVerificationByCanisterId' : IDL.Func(
+        [IDL.Principal],
+        [IDL.Opt(Verification)],
+        ['query'],
+      ),
+    'getVerifications' : IDL.Func([PaginationInfo], [Pagination_1], ['query']),
+    'registerVerification' : IDL.Func([RegisterVerification], [Result], []),
+    'saveBuildConfig' : IDL.Func([SaveBuildConfig], [], []),
+    'submitVerification' : IDL.Func([SubmitVerification], [], []),
   });
 };
-export const init = ({IDL}) => {
-  return [];
-};
+export const init = ({ IDL }) => { return []; };

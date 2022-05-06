d428712536473b70b90e4f85e5c68c6cdb95cacf
Scott Dn <scott.dn91@gmail.com>
2022-05-06 14:51:17 +0700

    feat: allow cors

--- src/httpResponse.ts
+++ src/httpResponse.ts
@@ -5,7 +5,10 @@ import {ErrorResponse, UnexpectedError} from "./error";
 const response = (statusCode: number, body?: unknown): APIGatewayProxyResultV2 => ({
   statusCode,
   headers: {
-    "Content-Type": "application/json"
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Headers": "Content-Type",
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
   },
   body: JSON.stringify(body)
 });
